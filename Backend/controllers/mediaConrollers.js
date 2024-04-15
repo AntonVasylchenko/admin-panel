dcb7be7d2b3a9cb77cde53d98fd0ac86820495e8
Vasylchenko Anton <87824792+Smd16Bi@users.noreply.github.com>
2024-04-15 06:13:33 +0300

    Working on uploading media

---
+++ Backend/controllers/mediaConrollers.js
@@ -0,0 +1,45 @@
+import { Media } from "../models/Media.js";
+import fs from "fs";
+import cloudinary from "cloudinary";
+
+export async function allMedia(req, res) {
+  try {
+    res.send("all media");
+  } catch (error) {
+    res.status(401).json({ msg: error.message });
+  }
+}
+export async function createMedia(req, res) {
+  try {
+    if (!req.files) {
+      throw new Error("Please provide media");
+    }
+    const media = req.files.media;
+    if (!media.mimetype.startsWith("image")) {
+      throw new Error("Please upload image or video");
+    }
+    const result = await cloudinary.v2.uploader.upload(
+      req.files.media.tempFilePath,
+      {
+        use_filename: true,
+        folder: "file",
+      }
+    );
+    fs.unlinkSync(req.files.media.tempFilePath);
+
+    await Media.create({ path: result.secure_url });
+
+    return res.status(201).json({
+      image: { src: result.secure_url },
+    });
+  } catch (error) {
+    res.status(401).json({ msg: error.message });
+  }
+}
+export async function deleteMedia(req, res) {
+  try {
+    res.send("Remove media");
+  } catch (error) {
+    res.status(401).json({ msg: error.message });
+  }
+}
