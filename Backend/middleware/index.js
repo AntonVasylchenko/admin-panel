46c5cd4257c939c2d7f2f47bb2f080525364b362
Vasylchenko Anton <87824792+Smd16Bi@users.noreply.github.com>
2024-05-18 21:56:46 +0300

    Remove mid

--- Backend/middleware/index.js
+++ Backend/middleware/index.js
@@ -1,4 +1,3 @@
-export { default as notFoundMiddleware } from "./notFound.js";
 export { default as errorHandlerMiddleware } from "./errorHandler.js";
 export { default as createLog } from "./createLog.js";
 export { default as aurhenticateMiddleware } from "./authentication.js";
